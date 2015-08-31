47fb2a642db0e2233b02a5185fabb1f76118cf5e
semaspahi <semaspahi@gmail.com>
2015-08-31 22:25:29 +0300

    Synced retrofit, recycler view, picasso and butterknife

---
+++ app/src/main/java/instagram/instagram/InstagramActivity.java
@@ -0,0 +1,14 @@
+package instagram.instagram;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class InstagramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instagram);
+    }
+
+}
---
+++ app/src/main/java/instagram/instagram/InstagramApp.java
@@ -0,0 +1,12 @@
+package instagram.instagram;
+
+import android.app.Application;
+
+
+public class InstagramApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
